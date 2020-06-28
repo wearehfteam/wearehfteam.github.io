3348d15e4119f8ac50b2aa937482442cc8ba9fee
tranthanhkhang2281999 <360tranthanhkhang2281999@gmail.com>
2020-06-28 17:45:17 +0700

    converted all functions to arrow functions, revised all variables's name, added content for result box

--- code/client/js/questions.js
+++ code/client/js/questions.js
@@ -1,206 +1,227 @@
-const questionText             = document.querySelector(".question-text");
-const inforText                = document.querySelector(".infor-text");
-const optionBox                = document.querySelector(".option-box");
-const resultBtn                = document.querySelector(".result-btn");
-const resultBox                = document.querySelector(".result-box");
-const showInforBtn             = document.querySelector(".more-infor-btn");
-const showInforBox             = document.querySelector(".flip-container");
-const correctAnswerDescription = document.querySelector(".correct-answer-description");
-const wrongAnswerDescription   = document.querySelector(".wrong-answer-description");
-const nextQuestionBtn          = document.querySelector(".next-question-btn");
-const resetBtn                 = document.querySelector(".reset-btn");
-const hideOptionBox            = document.querySelector(".option-box")
-const hidePlayBox              = document.querySelector(".card-box")
-const showOverBox              = document.querySelector(".over-box")
+const QUESTION_TEXT = document.querySelector('.question-text');
+const MORE_INFOR_TEXT = document.querySelector('.infor-text');
 
+const OPTIONS_BOX = document.querySelector('.option-box');
+const RESULT_BOX = document.querySelector('.result-box');
+const SHOW_MORE_INFOR_BOX = document.querySelector('.flip-container');
+const HIDE_OPTION_BOX = document.querySelector('.option-box');
+const HIDE_PLAY_BOX = document.querySelector('.card-box');
+const SHOW_OVER_BOX = document.querySelector('.over-box');
 
-let questionIndex  = 0;
+const NEXT_QUESTION_BTN = document.querySelector('.next-question-btn');
+
+const RESULT_BTN = document.querySelector('.result-btn');
+const RESET_BTN = document.querySelector('.reset-btn');
+const SHOW_MORE_INFOR_BTN = document.querySelector('.more-infor-btn');
+
+const CORRECT_ANSWER_DESCRIPTION = document.querySelector(
+  '.correct-answer-description'
+);
+const WRONG_ANSWER_DESCRIPTION = document.querySelector(
+  '.wrong-answer-description'
+);
+
+let category = 1;
+let listOfQuestions = [];
+let questionIndex = 0;
 let questionNumber = 0;
-var category       = 1;
-var myData         = null;
 
+let getRandAnsFromList = (listAnswers, answer, index) => {
+  let answers = listAnswers.filter((ques) => ques !== answer);
+  answers = answers.sort(() => Math.random() - Math.random()).slice(0, 4);
+  answers.sort(() => Math.random() - Math.random());
+  answers[index] = answer;
+  return answers;
+};
 
-function getRandAnsFromList(listAnswers, answer, index) {
-	var answers = listAnswers.filter(ques => ques !== answer)
-	    answers = answers.sort(() => Math.random() - Math.random()).slice(0, 4);
-	answers.sort(() => Math.random() - Math.random());
-	answers[index] = answer;
-	return answers;
-}
+let fetchListOfQuestions = async (category = 1) => {
+  let listOfQuestions = [];
+  try {
+    let response = await fetch(`http://localhost:3000/questions/${category}`);
+    let oriData = await response.json();
+    let listAnswers = oriData.map((ques) => ques.vietnamese);
 
-async function fetchData(category) {
-	try {
-		let response    = await fetch(`http://localhost:3000/questions/${category}`)
-		let oriData     = await response.json();
-		let listAnswers = oriData.map(ques => ques.vietnamese);
-		var questions   = await oriData.map(ques => {
-			var randIndex = Math.floor(Math.random() * 4);
-			return {
-				id      : ques.id,
-				question: ques.english,
-				moreInfo: ques.info,
-				indexAns: randIndex,
-				options : getRandAnsFromList(listAnswers, ques.vietnamese, randIndex)
-			}
-		})
-	} catch (error) {
-		console.log("Error in fetching data");
-		console.log(error);
+    listOfQuestions = await oriData.map((ques) => {
+      let randIndex = Math.floor(Math.random() * 4);
+      return {
+        id: ques.id,
+        question: ques.english,
+        options: getRandAnsFromList(listAnswers, ques.vietnamese, randIndex),
+        indexAns: randIndex,
+        moreInfo: ques.info,
+        status: 'not answer'
+      };
+    });
+  } catch (error) {
+    console.log('Error in fetching data');
+    console.log(error);
   }
-  console.log(questions);
-  
-	return questions
-}
 
-async function loadQuizPage() {
-	await fetchData(category).then(data => {
-		myData = data;
-	})
-	questionNumber++
-	questionText.innerHTML = myData[questionIndex].question;
-	inforText.innerHTML    = myData[questionIndex].moreInfo;
-	showFourAnswer();
+  return listOfQuestions;
 };
 
-function showFourAnswer() {
-	optionBox.innerHTML = "";
-	for (let i = 0; i < myData[questionIndex].options.length; i++) {
-		const option           = document.createElement("div");
-		      option.innerHTML = myData[questionIndex].options[i];
-		option.classList.add("option");
-		option.id = i;
-		option.setAttribute("onclick", "checkAnswer(this)");
-		optionBox.appendChild(option);
-	}
+let createNewQuestion = () => {
+  questionNumber++;
+  QUESTION_TEXT.innerHTML = listOfQuestions[questionIndex].question;
+  MORE_INFOR_TEXT.innerHTML = listOfQuestions[questionIndex].moreInfo;
+  createFourOptions();
 };
 
-function checkAnswer(ele) {
-  const id = ele.id;
-  if (id == myData[questionIndex].indexAns) {
-    ele.classList.add("correct");
+let createFourOptions = () => {
+  OPTIONS_BOX.innerHTML = '';
+  for (let i = 0; i < listOfQuestions[questionIndex].options.length; i++) {
+    const option = document.createElement('div');
+    option.innerHTML = listOfQuestions[questionIndex].options[i];
+    option.classList.add('option');
+    option.id = i;
+    option.setAttribute('onclick', 'checkAnswering(this)');
+    OPTIONS_BOX.appendChild(option);
+  }
+};
+
+let creatAnswersForResultBox = () => {
+  listOfQuestions.forEach((question, index) => {
+    let answerDiv = document.createElement('div');
+    let contentDiv = document.createElement('p');
+    let iconDiv = document.createElement('i');
+
+    answerDiv.innerHTML = `${index + 1}. ${question.question}`;
+
+    answerDiv.classList.add('answer');
+    iconDiv.classList.add('fas', 'fa-question');
+    // fas fa-times
+    // fas fa-question
+
+    answerDiv.appendChild(contentDiv);
+    answerDiv.append(iconDiv);
+
+    RESULT_BOX.appendChild(answerDiv);
+  });
+};
+
+let saveQuestionStatus = (status = 'not answer') => {
+  listOfQuestions[questionIndex].status = status;
+};
+
+let checkAnswering = (chosenOption) => {
+  const id = chosenOption.id;
+  let status = 'not answer';
+
+  if (id == listOfQuestions[questionIndex].indexAns) {
+    chosenOption.classList.add('correct');
     showCorrectAnswerNotification();
-    showNextQuestionBtn();
+
+    status = 'true';
   } else {
-    ele.classList.add("wrong");
+    chosenOption.classList.add('wrong');
     showWrongAnswerNotification();
-    showNextQuestionBtn();
-    for (let i = 0; i < optionBox.children.length; i++) {
-      if (optionBox.children[i].id == myData[questionIndex].indexAns) {
-        optionBox.children[i].classList.add("show-correct");
-      }
-    }
+
+    status = 'false';
+
+    // show true answer for user after he/she chose
+    const indexAns = listOfQuestions[questionIndex].indexAns;
+    OPTIONS_BOX.children[indexAns].classList.add('show-correct');
   }
-  disableOptions();
-  if(questionNumber == myData.length){
-    showOuizOverBox()
+
+  saveQuestionStatus(status);
+  disableAnsweringQuestion();
+
+  showNextQuestionBtn();
+
+  if (questionNumber == listOfQuestions.length) {
+    showOuizOverBox();
   }
 };
 
-
-function showWrongAnswerNotification() {
-	wrongAnswerDescription.classList.add("show");
+let showWrongAnswerNotification = () => {
+  WRONG_ANSWER_DESCRIPTION.classList.add('show');
 };
 
-function hideWrongAnswerNotification() {
-	correctAnswerDescription.classList.remove("show");
+let showCorrectAnswerNotification = () => {
+  CORRECT_ANSWER_DESCRIPTION.classList.add('show');
 };
 
-function showCorrectAnswerNotification() {
-	correctAnswerDescription.classList.add("show");
+let hideAnswerNotifications = () => {
+  CORRECT_ANSWER_DESCRIPTION.classList.remove('show');
+  WRONG_ANSWER_DESCRIPTION.classList.remove('show');
 };
 
-function hideCorrectAnswerNotification() {
-	wrongAnswerDescription.classList.remove("show");
+let hideMoreInfor = () => {
+  SHOW_MORE_INFOR_BOX.classList.remove('hover');
 };
 
-function hideMoreInfor() {
-	showInforBox.classList.remove("hover");
+let hideResultBox = () => {
+  RESULT_BOX.classList.remove('show-result-box');
 };
 
-function hideResultBox() {
-	resultBox.classList.remove("show-result-box");
+let disableAnsweringQuestion = () => {
+  for (let i = 0; i < OPTIONS_BOX.children.length; i++) {
+    OPTIONS_BOX.children[i].removeAttribute('onclick');
+  }
 };
 
-function disableOptions() {
-	for (let i = 0; i < optionBox.children.length; i++) {
-		optionBox.children[i].removeAttribute("onclick");
-	};
+let showNextQuestionBtn = () => {
+  NEXT_QUESTION_BTN.classList.remove('show');
 };
 
-function showNextQuestionBtn() {
-	nextQuestionBtn.classList.remove("show");
+let hideNextQuestionBtn = () => {
+  NEXT_QUESTION_BTN.classList.add('show');
 };
 
-function hideNextQuestionBtn() {
-	nextQuestionBtn.classList.add("show");
+let showOuizOverBox = () => {
+  HIDE_PLAY_BOX.classList.add('show-card-box');
+  SHOW_OVER_BOX.classList.remove('show-over-box');
 };
 
+let hideQuizOverBox = () => {
+  HIDE_PLAY_BOX.classList.remove('show-card-box');
+  SHOW_OVER_BOX.classList.add('show-over-box');
+};
 
-function showOuizOverBox(){
-  hidePlayBox.classList.add("show-card-box");
-  showOverBox.classList.remove("show-over-box")
-}
-function hideQuizOverBox(){
-  hidePlayBox.classList.remove("show-card-box");
-  showOverBox.classList.add("show-over-box")
-}
-
-//button nextquestion
-
-nextQuestionBtn.addEventListener("click", nextQuestion);
-
-function nextQuestion() {
+NEXT_QUESTION_BTN.addEventListener('click', () => {
   questionIndex++;
-  loadQuizPage();
+  createNewQuestion();
   hideMoreInfor();
-  hideCorrectAnswerNotification();
-  hideWrongAnswerNotification();
+  hideAnswerNotifications();
   hideNextQuestionBtn();
-};
+});
 
-// button reset
-resetBtn.addEventListener("click", resetAll);
-
-function resetAll() {
-  questionIndex  = 0;
-  questionNumber = 0
-  hideQuizOverBox()
-  loadQuizPage();
+RESET_BTN.addEventListener('click', () => {
+  questionIndex = 0;
+  questionNumber = 0;
+  hideQuizOverBox();
+  createNewQuestion();
   hideResultBox();
   hideMoreInfor();
-  hideCorrectAnswerNotification();
-  hideWrongAnswerNotification();
+  hideAnswerNotifications();
   hideNextQuestionBtn();
-};
+});
 
-// button result
-resultBtn.addEventListener("click", showResult);
+RESULT_BTN.addEventListener('click', () => {
+  RESULT_BOX.classList.toggle('show-result-box');
+});
 
-function showResult() {
-	resultBox.classList.toggle("show-result-box");
-};
+SHOW_MORE_INFOR_BTN.addEventListener('click', () => {
+  SHOW_MORE_INFOR_BOX.classList.toggle('hover');
+  console.log('hello');
+});
 
-// button more infor
-showInforBtn.addEventListener("click", showInfor);
-
-function showInfor() {
-	showInforBox.classList.toggle("hover");
+let shuffle = function (question) {
+  newQuestion = [];
+  while (question.length) {
+    indx = parseInt((Math.random() * 1000) % question.length);
+    newQuestion.push(question[indx]);
+    question.splice(indx, 1);
+    console.log(indx);
+  }
+  console.log(newQuestion);
+  return newQuestion;
 };
 
 window.onload = () => {
-  loadQuizPage();
+  fetchListOfQuestions(category).then((data) => {
+    listOfQuestions = data;
+    createNewQuestion();
+    creatAnswersForResultBox();
+  });
 };
-
-shuffle = function (question) {
-
-	newQuestion = [];
-	while (question.length) {
-		indx =parseInt(Math.random() * 1000 % question.length);
-		newQuestion.push(question[indx]);
-		question.splice(indx, 1)
-		console.log(indx)
-	}
-	console.log(newQuestion);
-	return newQuestion;
-  }
-
