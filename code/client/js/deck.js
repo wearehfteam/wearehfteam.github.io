f417b91254ccbd3e6841a10b5d9efea3a317df2a
SonUED <45086369+SonUED@users.noreply.github.com>
2020-06-30 20:47:14 +0700

    Update favicon and UI

--- code/client/js/deck.js
+++ code/client/js/deck.js
@@ -1,37 +1,23 @@
-
 async function fetchData() {
+  var res = await fetch("http://localhost:3000/decks");
+  var data = await res.json();
 
-    var res = await fetch("http://localhost:3000/decks")
-    var data = await res.json();
+  var deck = document.querySelector(".deck");
+  // var card = document.querySelector(".flip-card");
 
-    var  deck = document.querySelector(".deck");
-    var  card = document.querySelector(".flip-card");
-   
-
-    var  name = document.querySelector(".flip-card-back");
-    var  image = document.querySelector(".flip-card-front");
-    
-    console.log(data[0]);
-    
-    data.map(dt =>{
-    
-
-        deck.innerHTML+=`
+  // var name = document.querySelector(".flip-card-back");
+  // var image = document.querySelector(".flip-card-front");
+  data.map((dt) => {
+    deck.innerHTML += `
+         <a href="questions.html?${dt.id}">
         <div class="flip-card">
             <div class="flip-card-inner">
-            <a href="questions.html?${dt.id}">
-                <div class="flip-card-front">
-                    <img lass="flip-card-front" src="${dt.image}" alt="Avatar"
-                        style="width:300px;height:300px;border-radius: 50%;">
-                </div>
-                <div class="flip-card-back">
-                    <h1 id = "name" style="margin-top: 40%">${dt.name}</h1>
-                </div>
-            </a>
+              <img src="${dt.image}" alt="Avatar">
+             <div class="text">  <p>${dt.id}.${dt.name}</p>
             </div>
-        </div>`;
-    })
+        </div>
+           </a>`;
+  });
 }
-
 
 fetchData();
